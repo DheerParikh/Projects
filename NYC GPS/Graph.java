07d5ef566c778d527eb106c7e2a1e5e2926f8d6e
Dheer Parikh <dheerscience@gmail.com>
2025-06-30 01:46:06 -0400

    Update Graph.java

--- NYC GPS/Graph.java
+++ NYC GPS/Graph.java
@@ -1,1 +1,11 @@
+import java.util.*; 
+
+public interface Graph<T,W>{
+  public int getVertexCount();
+  public int getEdgesCount();
+  public boolean hasVertex(T vertex);
+  public boolean hasEdge(T source, T destination);
+  public void addVertex(T vertex);
+  public void addEdge(T source, T destination, W weight);
+} 
 
